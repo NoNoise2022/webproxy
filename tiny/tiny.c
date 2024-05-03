535af31ed9b716d381b2db9ae295690189c0c344
D_wW <99001026+NoNoise2022@users.noreply.github.com>
2024-05-03 16:21:57 +0900

    n01

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -42,12 +42,63 @@ int main(int argc, char **argv) {
   }
 }
 
+//client의 http요청을 처리하는 함수
 void doit(int fd)
 {
-  //
-  if (strcasecmp(method, "GET"))
-  {
-    clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't read the file");
+  int is_static;
+  struct stat sbuf;
+  char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+  char filename[MAXLINE], cgiargs[MAXLINE];
+  rio_t rio;
+
+   /* Read request line and headers */
+  Rio_readinitb(&rio, fd); 
+  Rio_readlineb(&rio, buf, MAXLINE); 
+  printf("Request headers:\n");
+  printf("%s", buf);
+  sscanf(buf, "%s %s %s", method, uri, version);
+
+  if (strcasecmp(method, "GET")){
+    clienterror(fd, method, "501", "Not Implemented",
+                "Tiny does not implement this method");
     return;
+  }
+  read_requesthdrs(&rio);
+
+  /* Parse URI from GET request */
+  is_static = parse_uri(uri, filename, cgiargs);
+  if (stat(filename, &sbuf) < 0){
+    clienterror(fd, filename, "404", "Not found",
+                "Tiny couldn't find this file");
+    return;
+  }
+
+  if (is_static)
+  { /* Serve static content */
+
+    //일반파일이 아니거나, 파일 읽기권한이 없는 경우 403 에러 안내 실행
+    if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode))
+    {
+      clienterror(fd, filename, "403", "Forbidden",
+                  "Tiny couldn't read the file");
+      return;
+    }
+    //일반파일, 읽기권한이 있다면, serve_static 실행
+    serve_static(fd, filename, sbuf.st_size);
+  }
+  //동적 컨텐츠를 요청 받았다면, 
+  else
+  { /* Serve dynamic content */
+    // S_IXUSR는 사용자(user)에 대한 실행 권한을 나타내는 상수
+    // st_mode는 stat 구조체에서 파일의 모드를 나타내는 필드
+    // 일반 파일이 아니거나, 실행가능한 파일이 아니거나, 실행 권한이 없는 경우 403에러 안내실행
+    if (!(S_ISREG(sbuf.st_mode)) || !(S_IXUSR & sbuf.st_mode))
+    {
+      clienterror(fd, filename, "403", "Forbidden",
+                  "Tiny couldn't run the CGI program");
+      return;
+    }
+
+    serve_dynamic(fd, filename, cgiargs);
   }
 }
