ba7cb910199463a16e2511b4f3079cd35a918517
D_wW <99001026+NoNoise2022@users.noreply.github.com>
2024-05-06 11:23:52 +0900

    n10

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -4,12 +4,49 @@
 /* $begin adder */
 #include "csapp.h"
 
+int main(void) 
+{
+    char *buf, *p;
+    char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
+    
+    int n1=0, n2=0;
+
+    if ((buf = getenv("QUERY_STRING")) != NULL) 
+    { 
+	p = strchr(buf, '&'); 
+	*p = '\0';
+    strcpy(arg1, buf);
+    strcpy(arg2, p + 1);
+
+	n1 = atoi(arg1); 
+	n2 = atoi(arg2); 
+    }
+
+    sprintf(content, "QUERY_STRING=%s", buf);
+    sprintf(content, "%sWelcome to add.com: ");
+    sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
+    sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>", content, n1, n2, n1 + n2);
+    sprintf(content, "%sThanks for visiting!\r\n", content);
+
+    printf("Connection: close\r\n");
+    printf("Content-length: %d\r\n", (int)strlen(content));
+    printf("Content-type: text/html\r\n\r\n");
+    printf("%s", content);
+    fflush(stdout);
+
+    exit(0);
+}
+/* $end adder */
+
+
+
+/*
+#include "csapp.h"
 int main(void) {
     char *buf, *p, *q, *method;
     char arg1[MAXLINE], arg2[MAXLINE], content[MAXLINE];
     int n1=0, n2=0;
 
-    /* Extract the two arguments */
     if ((buf = getenv("QUERY_STRING")) != NULL) { // 환경 변수 QUERY_STRING에 URL 파라미터로 받은 값을 저장한다.
 	p = strchr(buf, '&'); // 파라미터가 2개 이므로 처음 &가 나오고 나서부터 자른다.
     // Exampl url
@@ -28,14 +65,12 @@ int main(void) {
 
     method = getenv("REQUEST_METHOD");
     if (strcasecmp(method, "HEAD")){
-        /* Make the response body */
         sprintf(content, "%sWelcome to add.com: ", content);
         sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
         sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>", 
             content, n1, n2, n1 + n2);
         sprintf(content, "%sThanks for visiting!\r\n", content);
     }
-    /* Generate the HTTP response */
     // 응답시 알아야하는 헤더가 content-length, content-type이므로 
     // content-length와 content-type을 출력해본다.
     printf("Connection: close\r\n");
@@ -43,7 +78,6 @@ int main(void) {
     printf("Content-type: text/html\r\n\r\n");
     printf("%s", content);
     fflush(stdout);
-
     exit(0);
 }
-/* $end adder */
+*/
