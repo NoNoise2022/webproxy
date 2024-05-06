94289828022f32594a6adf64d9f7e364d52f10e0
D_wW <99001026+NoNoise2022@users.noreply.github.com>
2024-05-06 14:36:07 +0900

    n13

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -15,11 +15,17 @@ int main(void)
     { 
 	p = strchr(buf, '&'); 
 	*p = '\0';
-    strcpy(arg1, buf);
-    strcpy(arg2, p + 1);
 
-	n1 = atoi(arg1); 
-	n2 = atoi(arg2); 
+
+    // strcpy(arg1, buf);
+    // strcpy(arg2, p + 1);
+
+	// n1 = atoi(arg1); 
+	// n2 = atoi(arg2); 
+
+    sscanf(buf, "number1 = %d", &n1);
+    sscanf(p+1, "number2 = %d", &n2);
+
     }
 
     sprintf(content, "QUERY_STRING=%s", buf);
