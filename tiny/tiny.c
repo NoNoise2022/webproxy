c2da1080511143480301f7f2eb9617611699e18b
D_wW <99001026+NoNoise2022@users.noreply.github.com>
2024-05-06 15:41:34 +0900

    fix1

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -11,7 +11,7 @@
 void doit(int fd);
 void read_requesthdrs(rio_t *rp);
 int parse_uri(char *uri, char *filename, char *cgiargs);
-void serve_static(int fd, char *filename, int filesize);
+void serve_static(int fd, char *filename, int filesize, char *method);
 void get_filetype(char *filename, char *filetype);
 void serve_dynamic(int fd, char *filename, char *cgiargs);
 void clienterror(int fd, char *cause, char *errnum, char *shortmsg,
