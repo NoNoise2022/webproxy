58530eb7cfbce28496c5199e5567fbbb1101f622
D_wW <99001026+NoNoise2022@users.noreply.github.com>
2024-05-08 18:33:54 +0900

    n1n1

--- proxy.c
+++ proxy.c
@@ -1,159 +1,254 @@
 #include <stdio.h>
+#include <signal.h>
+
 #include "csapp.h"
+#include "cache.h"
 
-/* 과제 조건: HTTP/1.0 GET 요청을 처리하는 기본 시퀀셜 프록시
+void *thread(void *vargp);
+void doit(int clientfd);
+void read_requesthdrs(rio_t *rp, void *buf, int serverfd, char *hostname, char *port);
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg);
+void parse_uri(char *uri, char *hostname, char *port, char *path);
 
-  클라이언트가 프록시에게 다음 HTTP 요청 보내면 
-  GET http://www.google.com:80/index.html HTTP/1.1 
-  프록시는 이 요청을 파싱해야한다 ! 호스트네임, www.google.com, /index.html 
-  
-  이렇게 프록시는 서버에게 다음 HTTP 요청으로 보내야함.
-  GET /index.html HTTP/1.0   
-  
-  즉, proxy는 HTTP/1.1로 받더라도 server에는 HTTP/1.0으로 요청해야함 
-*/
-
-/* Recommended max cache and object sizes */
-#define MAX_CACHE_SIZE 1049000
-#define MAX_OBJECT_SIZE 102400
-
-/* You won't lose style points for including this long line in your code */
+static const int is_local_test = 1; // 테스트 환경에 따른 도메인&포트 지정을 위한 상수 (0 할당 시 도메인&포트가 고정되어 외부에서 접속 가능)
 static const char *user_agent_hdr =
     "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
     "Firefox/10.0.3\r\n";
-static const char *new_version = "HTTP/1.0";
 
-/* Function prototyps => 함수 순서 상관이 없게됨 */
-void do_it(int fd);
-void do_request(int p_clientfd, char *method, char *uri_ptos, char *host);
-void do_response(int p_connfd, int p_clientfd);
-int parse_uri(char *uri, char *uri_ptos, char *host, char *port);
-int parse_responsehdrs(rio_t *rp, int length);
-
-int main(int argc, char **argv) { 
-  int listenfd, p_connfd;
-  char hostname[MAXLINE], port[MAXLINE];
+int main(int argc, char **argv)
+{
+  int listenfd, *clientfd;
+  char client_hostname[MAXLINE], client_port[MAXLINE];
   socklen_t clientlen;
   struct sockaddr_storage clientaddr;
+  pthread_t tid;
+  signal(SIGPIPE, SIG_IGN); // SIGPIPE 예외처리
 
-  /* Check command line args */
-  if (argc != 2) {  
+  rootp = (web_object_t *)calloc(1, sizeof(web_object_t));
+  lastp = (web_object_t *)calloc(1, sizeof(web_object_t));
+
+  if (argc != 2)
+  {
     fprintf(stderr, "usage: %s <port>\n", argv[0]);
     exit(1);
   }
-  /* 해당 포트 번호에 해당하는 듣기 소켓 식별자를 열어준다. */
-  listenfd = Open_listenfd(argv[1]);
 
-  /* 클라이언트의 요청이 올 때마다 새로 연결 소켓을 만들어 doit()호출 */
-  while(1) {
+  listenfd = Open_listenfd(argv[1]); // 전달받은 포트 번호를 사용해 수신 소켓 생성
+  while (1)
+  {
     clientlen = sizeof(clientaddr);
-    /* 클라이언트에게서 받은 연결 요청을 accept한다. p_connfd = proxy의 connfd*/
-    p_connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
-
-    /* 연결이 성공했다는 메세지를 위해. Getnameinfo를 호출하면서 hostname과 port가 채워진다.*/
-    Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);
-    printf("Accepted connection from (%s, %s)\n", hostname, port);
-    
-    do_it(p_connfd); 
-    Close(p_connfd);
+    clientfd = Malloc(sizeof(int));
+    *clientfd = Accept(listenfd, (SA *)&clientaddr, &clientlen); // 클라이언트 연결 요청 수신
+    Getnameinfo((SA *)&clientaddr, clientlen, client_hostname, MAXLINE, client_port, MAXLINE, 0);
+    printf("Accepted connection from (%s, %s)\n", client_hostname, client_port);
+    Pthread_create(&tid, NULL, thread, clientfd); // Concurrent 프록시
   }
-  return 0;
 }
 
-  /* 
-  Before Parsing (Client로부터 받은 Request Line)
-  => GET http://www.google.com:80/index.html HTTP/1.1
-
-  Result Parsing
-  => host = www.google.com
-  => port = 80
-  => uri_ptos = /index.html
-
-  After Parsing (Server로 보낼 Request Line)
-  => GET /index.html HTTP/11. 
-  */ 
-
-void do_it(int p_connfd){
-  int p_clientfd;
-  char buf[MAXLINE],  host[MAXLINE], port[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
-  char uri_ptos[MAXLINE];
-  rio_t rio;
-  
-  /* Read request line and headers from Client */
-  Rio_readinitb(&rio, p_connfd);            // rio 버퍼와 fd(proxy의 connfd)를 연결시켜준다. 
-  Rio_readlineb(&rio, buf, MAXLINE);  // 그리고 rio(==proxy의 connfd)에 있는 한 줄(응답 라인)을 모두 buf로 옮긴다. 
-  printf("Request headers to proxy:\n");
-  printf("%s", buf);
-  sscanf(buf, "%s %s %s", method, uri, version); // buf에서 문자열 3개를 읽어와 각각 method, uri, version이라는 문자열에 저장 
-
-  /* Parse URI from GET request */
-  // if (!(parse_uri(uri, uri_ptos, host, port)))
-  //   return -1;
-  parse_uri(uri, uri_ptos, host, port);
-
-  p_clientfd = Open_clientfd(host, port);             // p_clientfd = proxy의 clientfd (연결됨)
-  do_request(p_clientfd, method, uri_ptos, host);     // p_clientfd에 Request headers 저장과 동시에 server의 connfd에 쓰여짐
-  do_response(p_connfd, p_clientfd);        
-  Close(p_clientfd);                                  // p_clientfd 역할 끝
+void *thread(void *vargp)
+{
+  int clientfd = *((int *)vargp);
+  Pthread_detach(pthread_self());
+  Free(vargp);
+  doit(clientfd);
+  Close(clientfd);
+  return NULL;
 }
 
-/* do_request: proxy => server */
-void do_request(int p_clientfd, char *method, char *uri_ptos, char *host){
-  char buf[MAXLINE];
-  printf("Request headers to server: \n");     
-  printf("%s %s %s\n", method, uri_ptos, new_version);
+void doit(int clientfd)
+{
+  int serverfd, content_length;
+  char request_buf[MAXLINE], response_buf[MAXLINE];
+  char method[MAXLINE], uri[MAXLINE], path[MAXLINE], hostname[MAXLINE], port[MAXLINE];
+  char *response_ptr, filename[MAXLINE], cgiargs[MAXLINE];
+  rio_t request_rio, response_rio;
 
-  /* Read request headers */        
-  sprintf(buf, "GET %s %s\r\n", uri_ptos, new_version);     // GET /index.html HTTP/1.0
-  sprintf(buf, "%sHost: %s\r\n", buf, host);                // Host: www.google.com     
-  sprintf(buf, "%s%s", buf, user_agent_hdr);                // User-Agent: ~(bla bla)
-  sprintf(buf, "%sConnections: close\r\n", buf);            // Connections: close
-  sprintf(buf, "%sProxy-Connection: close\r\n\r\n", buf);   // Proxy-Connection: close
+  /* 1️⃣ -1) Request Line 읽기 [🙋‍♀️ Client -> 🚒 Proxy] */
+  Rio_readinitb(&request_rio, clientfd);
+  Rio_readlineb(&request_rio, request_buf, MAXLINE);
+  printf("Request headers:\n %s\n", request_buf);
 
-  /* Rio_writen: buf에서 p_clientfd로 strlen(buf) 바이트로 전송*/
-  Rio_writen(p_clientfd, buf, (size_t)strlen(buf)); // => 적어주는 행위 자체가 요청하는거야~@!@!
-}
+  // 요청 라인 parsing을 통해 `method, uri, hostname, port, path` 찾기
+  sscanf(request_buf, "%s %s", method, uri);
+  parse_uri(uri, hostname, port, path);
 
-/* do_response: server => proxy */
-void do_response(int p_connfd, int p_clientfd){
-  char buf[MAX_CACHE_SIZE];
-  ssize_t n;
-  rio_t rio;
+  // Server에 전송하기 위해 요청 라인의 형식 변경: `method uri version` -> `method path HTTP/1.0`
+  sprintf(request_buf, "%s %s %s\r\n", method, path, "HTTP/1.0");
 
-  Rio_readinitb(&rio, p_clientfd);  //
-  n = Rio_readnb(&rio, buf, MAX_CACHE_SIZE);   // robust ...~ MAXLINE까지 일단 다 읽음 
-  Rio_writen(p_connfd, buf, n);
-}
-
-/* parse_uri: Parse URI from (Client로 부터 받은) GET request, Proxy에서 Server로의 GET request를 하기 위해 필요 */
-int parse_uri(char *uri, char *uri_ptos, char *host, char *port){ 
-  char *ptr;
-
-  if (!(ptr = strstr(uri, "://")))
-    return -1;
-  ptr += 3;                       
-  strcpy(host, ptr);              // host = www.google.com:80/index.html
-
-  if((ptr = strchr(host, ':'))){  // strchr(): 문자 하나만 찾는 함수 (''작은따옴표사용)
-    *ptr = '\0';                  // host = www.google.com
-    ptr += 1;
-    strcpy(port, ptr);            // port = 80/index.html
+  // 지원하지 않는 method인 경우 예외 처리
+  if (strcasecmp(method, "GET") && strcasecmp(method, "HEAD"))
+  {
+    clienterror(clientfd, method, "501", "Not implemented", "Tiny does not implement this method");
+    return;
   }
-  else{
-    if((ptr = strchr(host, '/'))){
-      *ptr = '\0';
-      ptr += 1;
+
+  // 현재 요청이 캐싱된 요청(path)인지 확인
+  web_object_t *cached_object = find_cache(path);
+  if (cached_object) // 캐싱 되어있다면
+  {
+    send_cache(cached_object, clientfd); // 캐싱된 객체를 Client에 전송
+    read_cache(cached_object);           // 사용한 웹 객체의 순서를 맨 앞으로 갱신
+    return;                              // Server로 요청을 보내지 않고 통신 종료
+  }
+
+
+  /* 1️⃣ -2) Request Line 전송 [🚒 Proxy -> 💻 Server] */
+  // Server 소켓 생성
+  serverfd = is_local_test ? Open_clientfd(hostname, port) : Open_clientfd("52.79.234.188", port);
+  if (serverfd < 0)
+  {
+    clienterror(serverfd, method, "502", "Bad Gateway", "📍 Failed to establish connection with the end server");
+    return;
+  }
+  Rio_writen(serverfd, request_buf, strlen(request_buf));
+
+  /* 2️⃣ Request Header 읽기 & 전송 [🙋‍♀️ Client -> 🚒 Proxy -> 💻 Server] */
+  read_requesthdrs(&request_rio, request_buf, serverfd, hostname, port);
+
+  /* 3️⃣ Response Header 읽기 & 전송 [💻 Server -> 🚒 Proxy -> 🙋‍♀️ Client] */
+  Rio_readinitb(&response_rio, serverfd);
+  while (strcmp(response_buf, "\r\n"))
+  {
+    Rio_readlineb(&response_rio, response_buf, MAXLINE);
+    if (strstr(response_buf, "Content-length")) // Response Body 수신에 사용하기 위해 Content-length 저장
+      content_length = atoi(strchr(response_buf, ':') + 1);
+    Rio_writen(clientfd, response_buf, strlen(response_buf));
+  }
+
+  /* 4️⃣ Response Body 읽기 & 전송 [💻 Server -> 🚒 Proxy -> 🙋‍♀️ Client] */
+  response_ptr = malloc(content_length);
+  Rio_readnb(&response_rio, response_ptr, content_length);
+  Rio_writen(clientfd, response_ptr, content_length); // Client에 Response Body 전송
+
+  if (content_length <= MAX_OBJECT_SIZE) // 캐싱 가능한 크기인 경우
+  {
+    // `web_object` 구조체 생성
+    web_object_t *web_object = (web_object_t *)calloc(1, sizeof(web_object_t));
+    web_object->response_ptr = response_ptr;
+    web_object->content_length = content_length;
+    strcpy(web_object->path, path);
+    write_cache(web_object); // 캐시 연결 리스트에 추가
+  }
+  else
+    free(response_ptr); // 캐싱하지 않은 경우만 메모리 반환
+
+  Close(serverfd);
+}
+
+// 클라이언트에 에러를 전송하는 함수
+// cause: 오류 원인, errnum: 오류 번호, shortmsg: 짧은 오류 메시지, longmsg: 긴 오류 메시지
+void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg)
+{
+  char buf[MAXLINE], body[MAXBUF];
+
+  // 에러 Bdoy 생성
+  sprintf(body, "<html><title>Tiny Error</title>");
+  sprintf(body, "%s<body bgcolor="
+                "ffffff"
+                ">\r\n",
+          body);
+  sprintf(body, "%s%s: %s\r\n", body, errnum, shortmsg);
+  sprintf(body, "%s<p>%s: %s\r\n", body, longmsg, cause);
+  sprintf(body, "%s<hr><em>The Tiny Web server</em>\r\n", body);
+
+  // 에러 Header 생성 & 전송
+  sprintf(buf, "HTTP/1.0 %s %s\r\n", errnum, shortmsg);
+  Rio_writen(fd, buf, strlen(buf));
+  sprintf(buf, "Content-type: text/html\r\n");
+  Rio_writen(fd, buf, strlen(buf));
+  sprintf(buf, "Content-length: %d\r\n\r\n", (int)strlen(body));
+  Rio_writen(fd, buf, strlen(buf));
+
+  // 에러 Body 전송
+  Rio_writen(fd, body, strlen(body));
+}
+
+// uri를 `hostname`, `port`, `path`로 파싱하는 함수
+// uri 형태: `http://hostname:port/path` 혹은 `http://hostname/path` (port는 optional)
+void parse_uri(char *uri, char *hostname, char *port, char *path)
+{
+  // host_name의 시작 위치 포인터: '//'가 있으면 //뒤(ptr+2)부터, 없으면 uri 처음부터
+  char *hostname_ptr = strstr(uri, "//") ? strstr(uri, "//") + 2 : uri;
+  char *port_ptr = strchr(hostname_ptr, ':'); // port 시작 위치 (없으면 NULL)
+  char *path_ptr = strchr(hostname_ptr, '/'); // path 시작 위치 (없으면 NULL)
+  strcpy(path, path_ptr);
+
+  if (port_ptr) // port 있는 경우
+  {
+    strncpy(port, port_ptr + 1, path_ptr - port_ptr - 1); 
+    strncpy(hostname, hostname_ptr, port_ptr - hostname_ptr);
+  }
+  else // port 없는 경우
+  {
+    if (is_local_test)
+      strcpy(port, "80"); // port의 기본 값인 80으로 설정
+    else
+      strcpy(port, "8000");
+    strncpy(hostname, hostname_ptr, path_ptr - hostname_ptr);
+  }
+}
+
+// Request Header를 읽고 Server에 전송하는 함수
+// 필수 헤더가 없는 경우에는 필수 헤더를 추가로 전송
+void read_requesthdrs(rio_t *request_rio, void *request_buf, int serverfd, char *hostname, char *port)
+{
+  int is_host_exist;
+  int is_connection_exist;
+  int is_proxy_connection_exist;
+  int is_user_agent_exist;
+
+  Rio_readlineb(request_rio, request_buf, MAXLINE); // 첫번째 줄 읽기
+  while (strcmp(request_buf, "\r\n"))
+  {
+    if (strstr(request_buf, "Proxy-Connection") != NULL)
+    {
+      sprintf(request_buf, "Proxy-Connection: close\r\n");
+      is_proxy_connection_exist = 1;
     }
-    strcpy(port, "80"); 
+    else if (strstr(request_buf, "Connection") != NULL)
+    {
+      sprintf(request_buf, "Connection: close\r\n");
+      is_connection_exist = 1;
+    }
+    else if (strstr(request_buf, "User-Agent") != NULL)
+    {
+      sprintf(request_buf, user_agent_hdr);
+      is_user_agent_exist = 1;
+    }
+    else if (strstr(request_buf, "Host") != NULL)
+    {
+      is_host_exist = 1;
+    }
+
+    Rio_writen(serverfd, request_buf, strlen(request_buf)); // Server에 전송
+    Rio_readlineb(request_rio, request_buf, MAXLINE);       // 다음 줄 읽기
   }
 
-  if ((ptr = strchr(port, '/'))){ // port = 80/index.html
-    *ptr = '\0';                  // port = 80
-    ptr += 1;     
-    strcpy(uri_ptos, "/");        // uri_ptos = /
-    strcat(uri_ptos, ptr);        // uri_ptos = /index.html
-  }  
-  else strcpy(uri_ptos, "/");
+  // 필수 헤더 미포함 시 추가로 전송
+  if (!is_proxy_connection_exist)
+  {
+    sprintf(request_buf, "Proxy-Connection: close\r\n");
+    Rio_writen(serverfd, request_buf, strlen(request_buf));
+  }
+  if (!is_connection_exist)
+  {
+    sprintf(request_buf, "Connection: close\r\n");
+    Rio_writen(serverfd, request_buf, strlen(request_buf));
+  }
+  if (!is_host_exist)
+  {
+    if (!is_local_test)
+      hostname = "52.79.234.188";
+    sprintf(request_buf, "Host: %s:%s\r\n", hostname, port);
+    Rio_writen(serverfd, request_buf, strlen(request_buf));
+  }
+  if (!is_user_agent_exist)
+  {
+    sprintf(request_buf, user_agent_hdr);
+    Rio_writen(serverfd, request_buf, strlen(request_buf));
+  }
 
-  return 0; // function int return => for valid check
+  sprintf(request_buf, "\r\n"); // 종료문
+  Rio_writen(serverfd, request_buf, strlen(request_buf));
+  return;
 }
